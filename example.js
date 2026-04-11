18bd4bf4afe7827436bd91704228f1849a410516
Murtuza Kainan <murtuzakainan@gmail.com>
2026-04-10 23:03:01 -0400

    Remove newFunction

--- example.js
+++ example.js
@@ -1,11 +1,5 @@
-function newFunction(value) {
-  console.log('This is new and improved');
-  console.log(`Value: ${value}`);
-  return value * 2;
-}
-
 function anotherFunction() {
   return 'Hello world';
 }
 
-module.exports = { newFunction, anotherFunction };
+module.exports = { anotherFunction };
